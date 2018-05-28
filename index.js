62d251e61d5839554314980c58dcc43ef969214c
Rahul <rahulsept2191@gmail.com>
2018-05-28 23:16:03 +0530

    Fixed webcam

--- index.js
+++ index.js
@@ -1,44 +1,56 @@
-var express = require('express'),
-  http = require('http'),
-  multer = require('multer'),
-  upload = multer({dest: 'uploads/'}),
-  exphbs = require('express-handlebars'),
-  easyimg = require('easyimage'),
-  _ = require('lodash'),
-  fs = require('fs'),
-  path = require('path'),
-  cv = require('opencv4nodejs');
+import express from 'express';
+import http from 'http';
+import exphbs from 'express-handlebars';
+import fs from 'fs';
+import path from 'path';
+import cv from 'opencv4nodejs';
+import io from 'socket.io';
+import  { grabFrames } from './lib/utils';
+import  { makeRunVideoFaceDetection, saveFaceImages, detectFaces } from './lib/commons';
 
-const { runVideoFaceDetection } = require('./commons');
-const classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_ALT2);
 const webcamPort = 0;
 const port = 3000;
-let image;
+const app = express();
 
-var app = express();
 app.use(express.static(__dirname + '/public'))
 app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'default'}));
+app.set('views', 'src/views');
 app.set('view engine', '.hbs');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+
 app.get('/', function(req, res, next) {
   return res.render('index');
 });
 
-function detectFaces(img) {
-  const options = {
-    minSize: new cv.Size(100, 100),
-    scaleFactor: 1.2,
-    minNeighbors: 10
-  };
-  return classifier.detectMultiScale(img.bgrToGray(), options).objects;
-}
 
+const httpServer = http.Server(app);
+const ioSocket = io(httpServer);
 
-http.listen(port, function(server) {
+httpServer.listen(port, function(server) {
   console.log('Listening on port %d', port);
 });
 
-io.on('connection', function(socket){
-  runVideoFaceDetection(webcamPort, detectFaces, socket);
+const runDetection = makeRunVideoFaceDetection();
+let maxFaceImageCount = 5;
+ioSocket.on('connection', function(socket) {
+  grabFrames(webcamPort, 1, function(frame) {
+    if (maxFaceImageCount > 0) {
+      saveFaceImages(frame, detectFaces);
+      maxFaceImageCount--;
+    }
+
+    const outBase64 = cv.imencode('.jpg', runDetection(frame, detectFaces)).toString('base64');
+    const buf = Buffer.from(outBase64, 'base64');
+    socket.emit('frame', {buffer: buf});
+  });
 });
+
+// const { makeRunDetectFacenetSSD } = require('./commons');
+// const runDetection = makeRunDetectFacenetSSD();
+//
+// ioSocket.on('connection', function(socket){
+//   grabFrames(webcamPort, 1, function(frame) {
+//     const outBase64 = cv.imencode('.jpg', runDetection(frame, 0.7)).toString('base64');
+//     var buf = Buffer.from(outBase64, 'base64');
+//     socket.emit('frame', { buffer: buf });
+//   });
+// });
